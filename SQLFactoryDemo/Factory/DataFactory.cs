bd1cd17588ee4fee59197f1fa3c960ab43868b32
wangqilong1225 <664660754@qq.com>
2020-12-20 10:23:53 +0800

    抽象工厂+简单工厂

---
+++ SQLFactoryDemo/Factory/DataFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLFactoryDemo.Interface;
+
+namespace SQLFactoryDemo.Factory
+{
+    public class DataFactory
+    {
+        public static IFactory GetDataDBFactory(string DBName) {
+            IFactory factory = null;
+            switch (DBName)
+            {
+                case "SQLServer":
+                    factory = new SQLServerFactory();
+                    break;
+                case "MySQL":
+                    factory = new MySQLFactory();
+                    break;
+                default:
+                    throw new Exception("暂不兼容其他数据库!");
+            }
+            return factory;
+        }
+    }
+}
--- SQLFactoryDemo/Program.cs
+++ SQLFactoryDemo/Program.cs
@@ -10,19 +10,27 @@ namespace SQLFactoryDemo
     {
         static void Main(string[] args)
         {
+            #region 抽象工厂
+            //string db = "SQLServer";
+            //IFactory factory = null;
+            //switch (db)
+            //{
+            //    case "SQLServer":
+            //        factory = new SQLServerFactory();
+            //        break;
+            //    case "MySQL":
+            //        factory = new MySQLFactory();
+            //        break;
+            //    default:
+            //        throw new Exception("暂不兼容其他数据库!");
+            //} 
+            #endregion
+
+            #region 抽象工厂+简单工厂
             string db = "SQLServer";
-            IFactory factory = null;
-            switch (db)
-            {
-                case "SQLServer":
-                    factory = new SQLServerFactory();
-                    break;
-                case "MySQL":
-                    factory = new MySQLFactory();
-                    break;
-                default:
-                    throw new Exception("暂不兼容其他数据库!");
-            }
+            IFactory factory = DataFactory.GetDataDBFactory(db); 
+            #endregion
+
 
             IUser user = factory.CreatUser();
             user.Insert(new User
@@ -31,7 +39,6 @@ namespace SQLFactoryDemo
                 Name = ""
             });
             user.GetUser(1);
-
 
             //反射
             //IUser user = (IUser)Assembly.Load("SQLFactoryDemo").CreateInstance("SQLFactoryDemo.Interface.SQLServerUser");
